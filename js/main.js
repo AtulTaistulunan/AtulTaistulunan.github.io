8f0cac85d2b7cb6397dbb7b73a81eaf1ddfee881
Jack <jack4258963@gmail.com>
2022-07-11 23:04:45 +0800

    push page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-document.addEventListener('DOMContentLoaded',function(){
-    
-    window.addEventListener('resize',function(){
-        windowWidth = window.innerWidth;
-        windowHeight = window.innerHeight;
-        console.log(windowWidth + ',' + windowHeight);
-        if(window.innerWidth <= 900){
-            // console.log('HI');
-            // var x = document.getElementById('test');
-            // x.textContent = "0.0";
-        }
-    });
-});
-
-// 手機版選單被點擊
+//手機版選單被點擊
 document.addEventListener('DOMContentLoaded',function(){
     var mobileNav = document.getElementById('navLine');
     var showContent = document.getElementById('navContent');

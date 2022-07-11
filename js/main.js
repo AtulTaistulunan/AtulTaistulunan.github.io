d29f38d0efc460f137bd93eaf304ff566c2073a7
Jack <jack4258963@gmail.com>
2022-07-11 21:57:38 +0800

    push pages

--- js/main.js
+++ js/main.js
@@ -15,7 +15,51 @@ document.addEventListener('DOMContentLoaded',function(){
 // 手機版選單被點擊
 document.addEventListener('DOMContentLoaded',function(){
     var mobileNav = document.getElementById('navLine');
+    var showContent = document.getElementById('navContent');
+
     mobileNav.addEventListener('click',function(){
         this.classList.toggle('mobileNavClick');
+        showContent.classList.toggle('showContent');
     });
 });
+
+//手機版隱藏區
+document.addEventListener('DOMContentLoaded',function(){
+    
+    var windowWidth = window.innerWidth;
+
+    resizeScreen(windowWidth);
+
+    window.addEventListener('resize',function(){
+        windowWidth = window.innerWidth;
+        resizeScreen(windowWidth);
+    });
+});
+
+function resizeScreen(windowWidth) {
+    var objWeb = document.getElementsByName('web');
+    var objMobile = document.getElementsByName('mobile');
+
+    if(windowWidth < 768){
+        //隱藏電腦
+        hideEle(objWeb);
+        //顯示手機
+        showEle(objMobile);
+    }else{
+        showEle(objWeb);
+        hideEle(objMobile);
+    }
+}
+
+function hideEle(obj){
+    for(var i =0;i < obj.length ;i++){
+        var x = obj[i];
+        x.style.visibility = 'collapse';
+    }
+}
+function showEle(obj){
+    for(var i =0;i < obj.length ;i++){
+        var x = obj[i];
+        x.style.visibility = 'visible';
+    }
+}
